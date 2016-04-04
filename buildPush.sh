2736e93087eead459c9f0ed5122b0217cb64adfa
sebastian <baschdi@wondrous.ch>
2016-04-04 12:13:10 +0200

    removed dumps, added content encoding

---
+++ buildPush.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
+
+make deps build
+make deps docker
+docker build --rm=true -t ribase/drone-s3-sync .
+docker push ribase/drone-s3-sync
