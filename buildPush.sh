20220007a794933c49c5973a266164e64dbf2f23
sebastian <baschdi@wondrous.ch>
2016-04-04 12:20:21 +0200

    final commit for pull request

--- buildPush.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export GOBIN=$GOPATH/bin
-export PATH=$PATH:$GOBIN
-
-make deps build
-make deps docker
-docker build --rm=true -t ribase/drone-s3-sync .
-docker push ribase/drone-s3-sync
